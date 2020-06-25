51178f1239dbccb80d75161de88050978a164598
David Hong <davidhong_89@yahoo.com>
2020-06-25 11:25:40 -0400

    Add sign in component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import FormInput from '../form-input/form-input.component';
 
 import './sign-in.style.scss';
 
@@ -37,10 +38,10 @@ class SignIn extends React.Component {
                 <span> Sign in with your email and password </span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <input name='email' value={this.state.email} onChange={this.handleChange} required />
-                    <label>Email</label>
-                    <input name='password' type='password' value={this.state.password}  onChange={this.handleChange} required />
-                    <label>Password</label>
+                    <FormInput name='email' value={this.state.email} handleChange={this.handleChange} label='email' required />
+                    
+                    <FormInput name='password' type='password' value={this.state.password}  handleChange={this.handleChange} label='password' required />
+                  
 
                     <input type='submit' value='Submit' />
                 </form>
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SignIn from '../../components/sign-in/sign-in.component';
 
+
 import './sign-in-and-sign-up.style.scss'
 
 const SignInAndSignUpPage = () => (
