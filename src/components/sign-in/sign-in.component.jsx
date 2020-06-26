557ffc564db4eb4c3f89cf9a0360b0e2bf4e1b78
David Hong <davidhong_89@yahoo.com>
2020-06-25 23:11:56 -0400

    Add firebase auth

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component'
+import { signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.style.scss';
 
@@ -45,6 +46,7 @@ class SignIn extends React.Component {
                   
 
                     <CustomButton type='submit'> Sign In </CustomButton>
+                    <CustomButton onClick={ signInWithGoogle } > Sign in with Google </CustomButton>
                 </form>
             </div>
         )
