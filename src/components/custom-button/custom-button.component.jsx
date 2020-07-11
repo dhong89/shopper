32bf12c622d93a2aff164b78e941b09f80702aed
David Hong <davidhong_89@yahoo.com>
2020-07-11 11:10:40 -0400

    Add add item action and reducer to keep track of items in the cart

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 
 import './custom-button.style.scss'
 
-
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps}) => (
-    <button className={`${isGoogleSignIn ? 'google-sign-in' : '' } custom-button`} { ...otherProps }>
-        {children}
-    </button>
-
-)
+const CustomButton = ({
+    children,
+    isGoogleSignIn,
+    inverted,
+    ...otherProps
+}) => (
+        <button
+            className={`${inverted ? 'inverted' : ''} ${
+                isGoogleSignIn ? 'google-sign-in' : ''
+                } custom-button`}
+            {...otherProps}
+        >
+            {children}
+        </button>
+    );
 
 export default CustomButton;
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,8 @@
 import { CartActionType } from './cart.types';
 
 const INTIAL_STATE = {
-    hidden: true
+    hidden: true,
+    cartItems: []
 }
 
 const cartReducer = (state = INTIAL_STATE, action) => {
@@ -11,6 +12,11 @@ const cartReducer = (state = INTIAL_STATE, action) => {
                 ...state,
                 hidden: !state.hidden
             }
+            case CartActionType.ADD_ITEM:
+                return {
+                    ...state,
+                    cartItems: [...state.cartItems, action.payload]
+                }
             default:
                 return state;
     }
