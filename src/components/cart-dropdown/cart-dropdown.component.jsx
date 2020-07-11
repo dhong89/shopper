be4ae887a532dc12929af60dcb08cae6963663f0
David Hong <davidhong_89@yahoo.com>
2020-07-11 14:35:50 -0400

    Add toggle action whenever going to checkout page to hide cart dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,10 +5,11 @@ import { withRouter } from 'react-router-dom';
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { selectCartItems } from "../../redux/cart/cart.selectors.js";
-
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
+ 
 import './cart-dropdown.style.scss';
 
-const CartDropdown = ({cartItems, history }) => (
+const CartDropdown = ({cartItems, history, dispatch }) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
             { cartItems.length ? 
@@ -18,7 +19,12 @@ const CartDropdown = ({cartItems, history }) => (
                 : 
                 <span className='empty-message'> Your cart is empty </span>
             }
-            <CustomButton onClick={ () => history.push('/checkout')}> GO TO CHECKOUT </CustomButton>
+            <CustomButton onClick={ () => {
+                history.push('/checkout');
+                dispatch(toggleCartHidden())
+            }}> 
+                GO TO CHECKOUT 
+                </CustomButton>
         </div>
     </div>
 )
@@ -29,3 +35,4 @@ const mapStateToProps = (state) => ({
 })
 
 export default withRouter(connect(mapStateToProps)(CartDropdown));
+//connect passes the ditpach function to the component if a second arguement is not passed. So we can access the dispatch function through our props rather than using MDTP.
