2d77374ae6246aedd56b3c4af38ba856545b390b
David Hong <davidhong_89@yahoo.com>
2020-07-13 15:52:26 -0400

    Add remove and add item functionality to checkout item component

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,4 +1,4 @@
-import { CartActionType } from './cart.types';
+import CartActionType  from "./cart.types";
 
 
 export const toggleCartHidden = () => ({
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,31 +1,38 @@
-import { CartActionType } from './cart.types';
-import { addItemToCart } from './cart.utils';
+import CartActionType from "./cart.types";
+import { addItemToCart, removeItemFromCart } from "./cart.utils";
 
-const INTIAL_STATE = {
-    hidden: true,
-    cartItems: []
-}
+const INITIAL_STATE = {
+  hidden: true,
+  cartItems: [],
+};
 
-const cartReducer = (state = INTIAL_STATE, action) => {
-    switch (action.type){
-        case CartActionType.TOGGLE_CART_HIDDEN:
-            return {
-                ...state,
-                hidden: !state.hidden
-            }
-            case CartActionType.ADD_ITEM:
-                return {
-                    ...state,
-                    cartItems: addItemToCart(state.cartItems, action.payload)
-                }
-                case CartActionType.CLEAR_ITEM_FROM_CART:
-                    return {
-                        ...state,
-                        cartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
-                    }
-            default:
-                return state;
-    }
-}
+const cartReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case CartActionType.TOGGLE_CART_HIDDEN:
+      return {
+        ...state,
+        hidden: !state.hidden,
+      };
+    case CartActionType.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload),
+      };
+    case CartActionType.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload),
+      };
+    case CartActionType.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (cartItem) => cartItem.id !== action.payload.id
+        ),
+      };
+    default:
+      return state;
+  }
+};
 
 export default cartReducer;
