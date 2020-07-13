431b21a752f907bbaea95fc98b00c8f6ca33d4aa
David Hong <davidhong_89@yahoo.com>
2020-07-13 16:30:41 -0400

    Add persist to checkout page

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,22 @@
 import {combineReducers} from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer'
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    whiteList: ['cart']
+}
+//dont need to white list user since firebase is taking care of the authentication so the user persists. Need to add any reducers that we need to white list in order for the data to persist. 
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
-})
+});
+
+
+
+export default  persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,11 @@
-import {createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
+import logger from "redux-logger";
 
-import rootReducer from './root-reducer';
+import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
 
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-const store = createStore( rootReducer, applyMiddleware(...middlewares))
-
-export default store;
-
+export const persistor = persistStore(store);
