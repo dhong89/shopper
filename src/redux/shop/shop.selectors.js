c964691bd74bc066b138056d0c8b18fbd92f443b
David Hong <davidhong_89@yahoo.com>
2020-07-14 23:25:56 -0400

    Add back the hash table for collection id

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,6 +1,14 @@
 import { createSelector } from 'reselect';
 import memoize from "lodash.memoize";
 
+const COLLECTION_ID_MATCH ={
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5
+}
+
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
@@ -8,11 +16,9 @@ export const selectCollections = createSelector(
     shop => shop.collections
 )
 
-export const selectCollection = memoize((collectionUrlParam) =>
+export const selectCollection = collectionUrlParam =>
   createSelector(
     [selectCollections],
-    (collections) => 
-        collections[collectionUrlParam]
+    collections => collections.find( collection => collection.id === COLLECTION_ID_MATCH[collectionUrlParam])
   )
-);
 
