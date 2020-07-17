e4d5bb9330f89de58bd71ff2d4fe0bb0a7d7313e
David Hong <davidhong_89@yahoo.com>
2020-07-17 09:11:04 -0400

    Fix compile error in preview component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,38 +1,39 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 import { withRouter } from 'react-router-dom';
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
-import { selectCartItems } from "../../redux/cart/cart.selectors.js";
-import { toggleCartHidden } from '../../redux/cart/cart.actions';
- 
+import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from '../../redux/cart/cart.actions.js';
+
 import './cart-dropdown.style.scss';
 
-const CartDropdown = ({cartItems, history, dispatch }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
-            { cartItems.length ? 
-                cartItems.map( cartItem => (
+            {cartItems.length ? (
+                cartItems.map(cartItem => (
                     <CartItem key={cartItem.id} item={cartItem} />
                 ))
-                : 
-                <span className='empty-message'> Your cart is empty </span>
-            }
-            <CustomButton onClick={ () => {
-                history.push('/checkout');
-                dispatch(toggleCartHidden())
-            }}> 
-                GO TO CHECKOUT 
-                </CustomButton>
+            ) : (
+                    <span className='empty-message'>Your cart is empty</span>
+                )}
         </div>
+        <CustomButton
+            onClick={() => {
+                history.push('/checkout');
+                dispatch(toggleCartHidden());
+            }}
+        >
+            GO TO CHECKOUT
+    </CustomButton>
     </div>
-)
+);
 
-
-const mapStateToProps = (state) => ({
-    cartItems: selectCartItems(state)
-})
+const mapStateToProps = createStructuredSelector({
+    cartItems: selectCartItems
+});
 
 export default withRouter(connect(mapStateToProps)(CartDropdown));
-//connect passes the ditpach function to the component if a second arguement is not passed. So we can access the dispatch function through our props rather than using MDTP.
--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import  CollectionPreview  from '../collection-preview/collection-preview.component';
-import { selectCollections } from "../../redux/shop/shop.selectors";
+
+import CollectionPreview from '../collection-preview/collection-preview.component';
+
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
 import './collections-overview.style.scss';
 
@@ -15,7 +17,7 @@ const CollectionsOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
+  collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
